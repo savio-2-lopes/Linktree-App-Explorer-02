5ab4ebace31901b8ca214810d1decf067bcc304a
savio-2-lopes <savioaugulopes@gmail.com>
2022-07-10 14:59:51 -0300

    :rocket: Add dark mode

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function toggle_dark_mode() {
+  var app = document.getElementsByTagName("BODY")[0];
+  if (localStorage.lightMode == "dark") {
+    localStorage.lightMode = "light";
+    app.setAttribute("light-mode", "light");
+  } else {
+    localStorage.lightMode = "dark";
+    app.setAttribute("light-mode", "dark");
+  }
+}
